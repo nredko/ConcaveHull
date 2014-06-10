0303b8064e147896ab5d3633d8b01f50da1f9d17
nredko@home <nredko@gmail.com>
2014-06-11 00:08:33 +0700

    .

--- hull.js
+++ hull.js
@@ -1,20 +1,10 @@
 // concave hull
 
-Array.prototype.getUnique = function(){
-        var u = {}, a = [];
-        for(var i = 0, l = this.length; i < l; ++i){
-            if(u.hasOwnProperty(this[i].toString())) {
-                continue;
-            }
-            a.push(this[i]);
-            u[this[i].toString()] = 1;
-        }
-        return a;
-    }
-
 // get angle <prev - this - next>
 L.LatLng.prototype.getAngle = function (prev, next){
     if(next.x == this.x && next.y == this.y)
+        return 360.0;
+    if(next.x == prev.x && next.y == prev.y)
         return 360.0;
     angle = (Math.atan2(next.y - this.y, next.x - this.x) - Math.atan2(prev.y - this.y, prev.x - this.x)) * 180 / Math.PI;
     while (angle < 0)
@@ -40,45 +30,42 @@ Array.prototype.pointInPolygon = function (point) {
 Array.prototype.concaveHull = function(maxlen){
     if(this.length <= 3)
         return this;
-    var byX = this.slice(0);
-    byX.sort(function(a, b){ 
-        if(a.x > b.x)
-            return 1;
-        if(a.x < b.x)
-            return -1;
-        return 0;
-    });
-    var byY = this.slice(0);
-    byY.sort(function(a, b){ 
-        if(a.y > b.y)
-            return 1;
-        if(a.y < b.y)
-            return -1;
-        return 0;
-    });
-    var yy = [];
-    for(var i = 1; i < byY.length; i++)
-        yy[i - 1] = byY[i].y - byY[i - 1].y;
-    var str = "[";
-    for(var i = 1; i < byY.length; i++)
-        str += byY[i].name + ", ";
-    if(DEBUG) console.log(str + "]");    
-    if(DEBUG) console.log("byY: " + yy.join());
-    if(DEBUG) console.log("byY: " + yy.sort(function(a, b){ return (a - b);}));
-
+    var byY = this.slice();
+    byY.sort(function(a, b){ return b.y - a.y;});
+if(DEBUG){
+    var str = ""
+    for(var i = 0, l = byY.length; i < l; i++)
+        str +=  byY[i].name + ", "
+    console.log("points: "+str);
+}
     var hull = [];
 
     var start = byY[0];
+    if(DEBUG) console.log("start: "+start.name)
     hull.push(start);
     var prev = start;
+    var prevprev = {};
+    prevprev.y = start.y - 1;
+    prevprev.x = start.x;
     var next;
-return this;
     while(next != start){
         if(DEBUG)L.circleMarker(prev,{color:"#F00", radius:3, fillOpacity:1}).addTo(map);
-        byAngle = this.slice(0).sort(function(a, b){return a.getAngle(prev, b);})
+        byAngle = this.slice().sort(function(a, b){return prev.getAngle(prevprev, b) - prev.getAngle(prevprev, a);})
+
+    if(DEBUG){
+        console.log("from: "+start.name)
+        console.log("prev: "+prev.name)
+        var str = ""
+        for(var i = 0, l = byY.length; i < l; i++)
+            str +=  byAngle[i].name + ":"+prev.getAngle(prevprev, byAngle[i])+", "
+        console.log("points by angle: "+str);
+    }
+
+
         next = byAngle[0];
         hull.push(next);
         if(DEBUG) L.polyline([prev, next], {color: 'red'}).addTo(map);
+        prevprev = prev;
         prev = next;
     }
     return hull;
