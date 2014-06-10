5b3bccc7ff46181033157b7200263ed0b90439e5
nredko <nredko@gmail.com>
2014-06-10 19:27:58 +0700

    new hull version

--- hull.js
+++ hull.js
@@ -1,4 +1,17 @@
 // concave hull
+
+Array.prototype.getUnique = function(){
+        var u = {}, a = [];
+        for(var i = 0, l = this.length; i < l; ++i){
+            if(u.hasOwnProperty(this[i].toString())) {
+                continue;
+            }
+            a.push(this[i]);
+            u[this[i].toString()] = 1;
+        }
+        return a;
+    }
+
 // get angle <prev - this - next>
 L.LatLng.prototype.getAngle = function (prev, next){
     if(next.x == this.x && next.y == this.y)
@@ -27,9 +40,31 @@ Array.prototype.pointInPolygon = function (point) {
 Array.prototype.concaveHull = function(maxlen){
     if(this.length <= 3)
         return this;
-    byX = this.slice(0).sort(function(a, b){ return a.x < b.x;});
-    byY = this.slice(0).sort(function(a, b){ return a.y < b.y;});
-    if(DEBUG) console.log("loaded and sorted");
+    var byX = this.slice(0);
+    byX.sort(function(a, b){ 
+        if(a.x > b.x)
+            return 1;
+        if(a.x < b.x)
+            return -1;
+        return 0;
+    });
+    var byY = this.slice(0);
+    byY.sort(function(a, b){ 
+        if(a.y > b.y)
+            return 1;
+        if(a.y < b.y)
+            return -1;
+        return 0;
+    });
+    var yy = [];
+    for(var i = 1; i < byY.length; i++)
+        yy[i - 1] = byY[i].y - byY[i - 1].y;
+    var str = "[";
+    for(var i = 1; i < byY.length; i++)
+        str += byY[i].name + ", ";
+    if(DEBUG) console.log(str + "]");    
+    if(DEBUG) console.log("byY: " + yy.join());
+    if(DEBUG) console.log("byY: " + yy.sort(function(a, b){ return (a - b);}));
 
     var hull = [];
 
@@ -37,6 +72,7 @@ Array.prototype.concaveHull = function(maxlen){
     hull.push(start);
     var prev = start;
     var next;
+return this;
     while(next != start){
         if(DEBUG)L.circleMarker(prev,{color:"#F00", radius:3, fillOpacity:1}).addTo(map);
         byAngle = this.slice(0).sort(function(a, b){return a.getAngle(prev, b);})
